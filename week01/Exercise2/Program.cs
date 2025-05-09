637e2883df1368fea1ef4a64ffa3aa2c5c947c6f
Donfred270 <donfredvallon@gmail.com>
2025-05-09 15:33:49 -0400

    exercise 3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+        Console.WriteLine("what is the percentage of your grade? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+
+        //determine the sign of the grade
+        string gradeSign = "";
+        int gradeSignInt = gradeInt % 10; 
+        if (gradeSignInt >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (gradeSignInt < 3)
+        {
+            gradeSign = "-";
+        }
+         else
+        {
+            gradeSign = "";
+        }
+
+        //determine the letter grade
+        if (gradeInt >= 90 && gradeInt <= 100)
+        {
+            letter = "A";
+            Console.WriteLine($"You got an {letter}{gradeSign}!, you passed!");
+        }
+        else if (gradeInt >= 80 && gradeInt < 90)
+        {
+            letter = "B";
+            Console.WriteLine($"You got a {letter}{gradeSign}! Congratulations, you passed!");
+        }
+        else if (gradeInt >= 70 && gradeInt < 80)
+        {
+            letter = "C";
+            Console.WriteLine($"You got a {letter}{gradeSign}! Congratulations, you passed!");
+        }
+        else if (gradeInt >= 60 && gradeInt < 70)
+        {
+            letter = "D";
+            int basicGrade = 70 - gradeInt;
+            Console.WriteLine($"You got a {letter}{gradeSign}! you need to study more, you are {basicGrade} points away to passed this course.");
+        }
+        else if (gradeInt < 0 || gradeInt > 100)
+        {
+            Console.WriteLine("Invalid grade, please enter a number between 0 and 100.");
+            return;
+        }
+        else
+        {
+            letter = "F";
+            int basicGrade = 70 - gradeInt;
+            Console.WriteLine($"You got an {letter}{gradeSign}!");
+            Console.WriteLine($"You need to study more, you are {basicGrade} points away to passed this course.");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        play(); // Call the play method to start the game
+    }
+
+    static void  play() // Method to play the guessing game
+    // The method generates a random number and prompts the user to guess it
+    {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101); // Generates a random number between 1 and 10 (inclusive)
+        int guess = 0;
+        int test = 0; // Initialize try counter
+        string replay = "y";
+        do
+        {
+             test++; // Increment try counter
+            Console.WriteLine("What is the magic number between 1 - 10? ");
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out guess)) // Check if the input is a valid integer
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+            
+            else if (guess < 1 || guess > 101) // Check if the guess is within the valid range
+            {
+                Console.WriteLine("Please enter a number between 1 and 10.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("too low!, try again.");
+            }
+            else if (guess>number)
+            {
+                Console.WriteLine("too high!, try again");
+            }
+            else
+            {
+                Console.WriteLine("You guesses it, congratulations!");
+                Console.WriteLine($"You took {test} tries to guess the number.");
+                Console.WriteLine($"The magic number was {number}.");
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                replay = Console.ReadLine().ToLower(); // Convert input to lowercase for consistency
+                if (replay == "yes" || replay == "y")
+                {
+                   play(); // Restart the game by calling Main again
+                }
+                else if (replay == "no" || replay == "n")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    break; // Exit the loop and end the game
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+                }
+            }
+        }while(guess != number);
     }
 }
